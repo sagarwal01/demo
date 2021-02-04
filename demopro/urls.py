20e665f4ea0d497286cf6ac35c63abd571c42def
agarwal01 <sagarwal@connexial.co.in>
2021-02-04 11:49:55 +0530

    update

--- demopro/urls.py
+++ demopro/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from demoapp import views
 
 urlpatterns = [
+    re_path(r'^.*\.html', views.pages, name='pages'),
     path('admin/', admin.site.urls),
-    path('index/',views.index, name='index')
+    path('',views.index, name='index'),
+    path('home/', views.home, name='home')
 ]
