09fbf97baa137d8611a918ac3a5d7c7741a1c8f5
agarwal01 <sagarwal@connexial.co.in>
2021-02-04 11:05:58 +0530

    update

--- demopro/settings.py
+++ demopro/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'demopro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['demoapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
